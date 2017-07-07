635865b2f83ce4282493aa3de21e6e647b84a59b
ShongWT <Wtshong@gmail.com>
2017-07-07 22:56:36 +0800

    New Update

--- www/js/app.js
+++ www/js/app.js
@@ -26,51 +26,17 @@ angular.module('starter', ['ionic','angular-svg-round-progress','ngCordova'])
 
 .controller('mainCtrl', function($scope, $ionicModal, $timeout, $ionicPlatform) {
 
-        var QiGg_Lessions = [   {lesson: "1", step:  0, descr: "起势", duration: 10},
-                                {lesson: "1", step:  1, descr: "马步站桩", duration: 300},
-                                {lesson: "1", step:  2, descr: "右臂在上左臂在下，双臂平行", duration: 15},
-                                {lesson: "1", step:  3, descr: "两小臂由内向外翻转", duration: 3},
-                                {lesson: "1", step:  4, descr: "两手鱼际肌相对同时对准膻中穴", duration: 60},
-                                {lesson: "1", step:  5, descr: "两小臂由内向外翻转", duration: 30},
-                                {lesson: "1", step:  6, descr: "两手鱼际肌相对同时对准膻中穴", duration: 60},
-                                {lesson: "1", step:  7, descr: "两臂往上推, 推至鱼际肌对准鼻尖", duration: 60},
-                                {lesson: "1", step:  8, descr: "两手鱼际肌相对同时对准膻中穴", duration: 60},
-                                {lesson: "1", step:  9, descr: "双臂往下沉，至中脘穴稍停", duration: 10},
-                                {lesson: "1", step: 10, descr: "马步站桩式", duration: 60},
-                                {lesson: "1", step: 11, descr: "左臂在上右臂在下，双臂平行", duration: 10},
-                                {lesson: "1", step: 12, descr: "两小臂由内向外翻转", duration: 3},
-                                {lesson: "1", step: 13, descr: "两手鱼际肌相对同时对准膻中穴", duration: 60},
-                                {lesson: "1", step: 14, descr: "两小臂由内向外翻转", duration: 30},
-                                {lesson: "1", step: 15, descr: "两手鱼际肌相对同时对准膻中穴", duration: 60},
-                                {lesson: "1", step: 16, descr: "两臂往上推, 推至鱼际肌对准鼻尖", duration: 60},
-                                {lesson: "1", step: 17, descr: "两手鱼际肌相对同时对准膻中穴", duration: 60},
-                                {lesson: "1", step: 18, descr: "双臂往下沉，至中脘穴稍停", duration: 10},
-                                {lesson: "1", step: 19, descr: "马步站桩式", duration: 300},
-                                {lesson: "2", step:  0, descr: "起势", duration:  10},
-                                {lesson: "2", step:  1, descr: "马步站桩", duration: 300},
-                                {lesson: "2", step:  2, descr: "动作3",   duration: 10},
-                                {lesson: "2", step:  3, descr: "动作4",   duration: 30},
-                                {lesson: "2", step:  4, descr: "动作5",   duration: 60},
-                                {lesson: "2", step:  5, descr: "动作6",   duration: 30},
-                                {lesson: "2", step:  6, descr: "动作7",   duration: 60},
-                                {lesson: "2", step:  7, descr: "动作8",   duration: 30},
-                                {lesson: "2", step:  8, descr: "动作9",   duration: 60},
-                                {lesson: "2", step:  9, descr: "动作10",  duration: 30},
-                                {lesson: "2", step: 10, descr: "动作11",  duration: 10},
-                                {lesson: "2", step: 11, descr: "动作12",  duration: 60, gotoStep:3, repeat:1, complete:0},
-                                {lesson: "2", step: 12, descr: "End",  duration: 0}                                
-                                
-                            ];
-
+        var QiGg_Lessions = [];
         var curr_lesson = [];   
         // Timer
         var mytimeout = null; // the current timeoutID
         $scope.steps = 0;
         $scope.lesson = 0;
         $scope.TotalSteps=0;
-        $scope.repeat = 0;
+        $scope.totalRepeat = 0;
         $scope.gotoStep = 0;
         $scope.repeat_start = false;
+        $scope.lessonTitle = "";
         
         $ionicPlatform.ready(function() {
             if( window.plugins && window.plugins.NativeAudio ) {
@@ -118,7 +84,7 @@ angular.module('starter', ['ionic','angular-svg-round-progress','ngCordova'])
                     if ($scope.gotoStep > 0) {
                         $scope.step = $scope.gotoStep;
                         $scope.gotoStep = 0;
-                        $scope.repeat--;
+                        //$scope.repeat--;
                     } else {
                         $scope.step++;  
                     };
@@ -126,13 +92,24 @@ angular.module('starter', ['ionic','angular-svg-round-progress','ngCordova'])
                     if ($scope.step < curr_lesson.length) {
                         $scope.selectTimer(curr_lesson[$scope.step].duration);
                         $scope.startTimer(); 
-                        if (curr_lesson[$scope.step].gotoStep > 0 && curr_lesson[$scope.step].complete === 0 &&  curr_lesson[$scope.step].repeat > 0) {
+                        if (curr_lesson[$scope.step].gotoStep > 0 
+                                && curr_lesson[$scope.step].complete === 0 
+                                &&  curr_lesson[$scope.step].repeat > 0) {
+
                             $scope.gotoStep = curr_lesson[$scope.step].gotoStep; 
-                            curr_lesson[$scope.step].repeat--; 
+                            curr_lesson[$scope.step].repeat--;
+                            $scope.totalRepeat++;
+                            
+
                             if (curr_lesson[$scope.step].repeat == 0) {
                                 curr_lesson[$scope.step].complete = 1;
+                                
                             };
-                        } else {
+                        } else if (curr_lesson[$scope.step].complete === 1 
+                                &&  curr_lesson[$scope.step].repeat == 0) {
+                            $scope.totalRepeat++;
+                        }
+                        else {
                             $scope.gotoStep = 0;
                         };
                         // console.log( 'complete: ' + curr_lesson[$scope.step].complete );
@@ -142,7 +119,7 @@ angular.module('starter', ['ionic','angular-svg-round-progress','ngCordova'])
                     $scope.steps = 0;
                     $scope.lesson = 0;
                     $scope.TotalSteps=0;    
-                    $scope.repeat = 0;             
+                    //$scope.repeat = 0;             
                     };
                 } else {
                     $scope.$broadcast('timer-stopped', 0);
@@ -220,7 +197,18 @@ angular.module('starter', ['ionic','angular-svg-round-progress','ngCordova'])
             $scope.paused = false;
             $scope.done = false;
         };
-
+        $scope.backward  = function() {
+            $scope.stopTimer();
+            $scope.step--;
+            $scope.selectTimer(curr_lesson[$scope.step].duration);
+            $scope.startTimer(); 
+        };
+        $scope.forward  = function() {
+            $scope.stopTimer();
+            $scope.step++;
+            $scope.selectTimer(curr_lesson[$scope.step].duration);
+            $scope.startTimer(); 
+        };        
         // This function helps to display the time in a correct way in the center of the timer
         $scope.humanizeDurationTimer = function(input, units) {
             // units is a string with possible values of y, M, w, d, h, m, s, ms
@@ -244,6 +232,88 @@ angular.module('starter', ['ionic','angular-svg-round-progress','ngCordova'])
 
         $scope.startLesson = function(lesson_no) {
             $scope.step = 0;
+            $scope.totalRepeat =0;
+            QiGg_Lessions = [   {lesson: "0", step:  0, descr: "马步站桩", duration: 300},
+                                {lesson: "1", step:  0, descr: "起势", duration: 10},
+                                {lesson: "1", step:  1, descr: "马步站桩", duration: 300},
+                                {lesson: "1", step:  2, descr: "右臂在上左臂在下，双臂平行", duration: 15},
+                                {lesson: "1", step:  3, descr: "两小臂由内向外翻转", duration: 3},
+                                {lesson: "1", step:  4, descr: "两手鱼际肌相对同时对准膻中穴", duration: 60},
+                                {lesson: "1", step:  5, descr: "两小臂由内向外翻转", duration: 30},
+                                {lesson: "1", step:  6, descr: "两手鱼际肌相对同时对准膻中穴", duration: 60},
+                                {lesson: "1", step:  7, descr: "两臂往上推, 推至鱼际肌对准鼻尖", duration: 60},
+                                {lesson: "1", step:  8, descr: "两手鱼际肌相对同时对准膻中穴", duration: 60},
+                                {lesson: "1", step:  9, descr: "双臂往下沉，至中脘穴稍停", duration: 10},
+                                {lesson: "1", step: 10, descr: "马步站桩式", duration: 60},
+                                {lesson: "1", step: 11, descr: "左臂在上右臂在下，双臂平行", duration: 10},
+                                {lesson: "1", step: 12, descr: "两小臂由内向外翻转", duration: 3},
+                                {lesson: "1", step: 13, descr: "两手鱼际肌相对同时对准膻中穴", duration: 60},
+                                {lesson: "1", step: 14, descr: "两小臂由内向外翻转", duration: 30},
+                                {lesson: "1", step: 15, descr: "两手鱼际肌相对同时对准膻中穴", duration: 60},
+                                {lesson: "1", step: 16, descr: "两臂往上推, 推至鱼际肌对准鼻尖", duration: 60},
+                                {lesson: "1", step: 17, descr: "两手鱼际肌相对同时对准膻中穴", duration: 60},
+                                {lesson: "1", step: 18, descr: "双臂往下沉，至中脘穴稍停", duration: 10},                                
+                                {lesson: "1", step: 19, descr: "马步站桩式", duration: 300},
+                                {lesson: "1", step: 20, descr: "<round#>", duration: 1, gotoStep:2, repeat:1, durationOverride:1, complete:0} ,                                
+                                {lesson: "2", step:  0, descr: "起势",   duration: 10},
+                                {lesson: "2", step:  1, descr: "站桩",   duration: 300},
+                                {lesson: "2", step:  2, descr: "十字手", duration: 10},
+                                {lesson: "2", step:  3, descr: "小球1",  duration: 30},
+                                {lesson: "2", step:  4, descr: "大球2",  duration: 60},
+                                {lesson: "2", step:  5, descr: "小球3",  duration: 30},
+                                {lesson: "2", step:  6, descr: "大球4",  duration: 60},
+                                {lesson: "2", step:  7, descr: "小球5",  duration: 30},
+                                {lesson: "2", step:  8, descr: "大球6",  duration: 60},
+                                {lesson: "2", step:  9, descr: "小球7",  duration: 30},
+                                {lesson: "2", step: 10, descr: "转站桩", duration: 5},
+                                {lesson: "2", step: 11, descr: "站桩",   duration: 60},
+                                {lesson: "2", step: 12, descr: "十字手", duration: 10},
+                                {lesson: "2", step: 13, descr: "小球1",  duration: 30},
+                                {lesson: "2", step: 14, descr: "大球2",  duration: 60},
+                                {lesson: "2", step: 15, descr: "小球3",  duration: 30},
+                                {lesson: "2", step: 16, descr: "大球4",  duration: 60},
+                                {lesson: "2", step: 17, descr: "小球5",  duration: 30},
+                                {lesson: "2", step: 18, descr: "大球6",  duration: 60},
+                                {lesson: "2", step: 19, descr: "小球7",  duration: 30},
+                                {lesson: "2", step: 20, descr: "转站桩", duration: 5},                                                     
+                                {lesson: "2", step: 21, descr: "站桩",   duration: 300},
+                                {lesson: "2", step: 22, descr: "<round#>", duration: 1, gotoStep:2, repeat:1, durationOverride:300, complete:0},                                     
+                                {lesson: "3", step:  0, descr: "起势",   duration: 15},
+                                {lesson: "3", step:  1, descr: "站桩",   duration: 300},                                
+                                {lesson: "3", step:  2, descr: "2 食指扳上",  duration: 60},
+                                {lesson: "3", step:  3, descr: "4 无名指扳下",duration: 60},
+                                {lesson: "3", step:  4, descr: "1 大拇指扳下",duration: 60},
+                                {lesson: "3", step:  5, descr: "5 尾指扳下", duration:  60},
+                                {lesson: "3", step:  6, descr: "3 中指扳下", duration:  60},                                                   
+                                {lesson: "3", step:  7, descr: "<round#>", duration: 1, gotoStep:2, repeat:4, durationOverride:1, complete:0},
+                                {lesson: "3", step:  8, descr: "站桩", duration: 300},
+                                {lesson: "4", step:  0, descr: "马步站桩", duration: 300},
+                                {lesson: "4", step:  1, descr: "起势", duration: 10},
+                                {lesson: "4", step:  2, descr: "站桩", duration: 300}
+
+                            ];            
+            curr_lesson = [];
+
+            switch (lesson_no) {
+                case 0:
+                    $scope.lessonTitle="马步站桩";
+                    break;
+                case 1:
+                    $scope.lessonTitle="双臂揽月";
+                    break;
+                case 2:
+                    $scope.lessonTitle="双臂抱球";
+                    break;
+                case 3:
+                    $scope.lessonTitle="扶正益寿";
+                    break;
+                case 4:
+                    $scope.lessonTitle="滚球法";
+                    break;
+            };
+
+            // console.log(lesson_no);
+
             for (var i = 0; i < QiGg_Lessions.length; i++) {
                 if (QiGg_Lessions[i].lesson == lesson_no) {
                    curr_lesson.push(QiGg_Lessions[i]); 
@@ -255,9 +325,17 @@ angular.module('starter', ['ionic','angular-svg-round-progress','ngCordova'])
             $scope.selectTimer(curr_lesson[$scope.step].duration);
             $scope.lesson = lesson_no;
         };
+        
         $scope.getDescr = function() {
+
             if ($scope.step < curr_lesson.length) {
-                return curr_lesson[$scope.step].descr;    
+                var ls_desc = "";
+                
+                ls_desc = curr_lesson[$scope.step].descr;
+                if (ls_desc == "<round#>") {
+                  ls_desc = "第" + $scope.totalRepeat.toString() + "遍";
+                }
+                return ls_desc;    
             } else {
                 return "";
             }
